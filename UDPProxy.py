ca61b5319b9c9658d89598d6435ec57369e01bb8
Magnus-Hansen <54136904+Magnus-Hansen@users.noreply.github.com>
2024-11-28 13:42:16 +0100

    Added led adn temperature

--- UDPProxy.py
+++ UDPProxy.py
@@ -2,6 +2,7 @@ import time
 import requests
 import json
 from scd30_i2c import SCD30
+from gpiozero import LED
 
 headersArray = {'Content-type': 'application/json'}
 def post(postJson):
@@ -14,21 +15,26 @@ scd30.set_measurement_interval(2)
 scd30.start_periodic_measurement()
 
 time.sleep(2)
+led = LED(18)
 
-while True:
-    if scd30.get_data_ready():
-        m = scd30.read_measurement()
-        if m is not None:
-            #print(f"CO2: {m[0]:.2f}ppm, temp: {m[1]:.2f}'C, rh: {m[2]:.2f}%")
-            #print(m)
-            jsonM = json.dumps({"Humidity": m[2], "CO2": m[0]})
-            post(jsonM)
+try: 
+    while True:
+        if scd30.get_data_ready():
+            m = scd30.read_measurement()
+            if m is not None:
+                #print(f"CO2: {m[0]:.2f}ppm, temp: {m[1]:.2f}'C, rh: {m[2]:.2f}%")
+                #print(m)
+                jsonM = json.dumps({"Humidity": m[2], "CO2": m[0], "Temperature": m[1]})
+                post(jsonM)
+                
+                if m[0] < 1000:
+                    led.off()
 
-        time.sleep(10)
-    else:
-        time.sleep(0.2)
+                if m[0] >= 1000:
+                    led.on()
 
-
-
-
-
+            time.sleep(10)
+        else:
+            time.sleep(0.2)
+except KeyboardInterrupt:
+    print('Interrupted')
